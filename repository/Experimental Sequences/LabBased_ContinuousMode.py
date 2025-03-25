41b5bbc08a0e94e13990c3555ffe6bf359c3fab5
alkalineteam <alkalineteam88@gmail.com>
2025-03-25 14:56:19 +0000

    Set Default Values for the experiment

--- repository/Experimental Sequences/LabBased_ContinuousMode.py
+++ repository/Experimental Sequences/LabBased_ContinuousMode.py
@@ -15,7 +15,7 @@ class Everything_ON(EnvExperiment):
         self.probe_shutter:TTLOut=self.get_device("ttl7")
         self.camera_trigger:TTLOut=self.get_device("ttl8")
         self.clock_shutter:TTLOut=self.get_device("ttl9")
-        
+
         # self.pmt_shutter:TTLOut=self.get_device("ttl10")
         # self.camera_trigger:TTLOut=self.get_device("ttl11")
         # self.camera_shutter:TTLOut=self.get_device("ttl12")        
@@ -23,7 +23,7 @@ class Everything_ON(EnvExperiment):
         self.red_mot_aom = self.get_device("urukul0_ch0")
         self.blue_mot_aom = self.get_device("urukul0_ch1")
         self.zeeman_slower_aom = self.get_device("urukul0_ch2")
-        self.probe_aom = self.get_device("urukul0_ch2")
+        self.probe_aom = self.get_device("urukul0_ch3")
         #AD9912
         self.lattice_aom=self.get_device("urukul1_ch0")
         self.stepping_aom_aom=self.get_device("urukul1_ch1")
@@ -38,23 +38,26 @@ class Everything_ON(EnvExperiment):
         self.setattr_argument("Cycle", NumberValue(default = 50))
 
         self.setattr_argument("BMOT_Frequency", NumberValue(default = 90.0))
-        self.setattr_argument("BMOT_Amplitude", NumberValue(default = 0.07))
+        self.setattr_argument("BMOT_Amplitude", NumberValue(default = 0.1))
         # self.setattr_argument("BMOT_Attenuation", NumberValue(default = 0.0))
 
         self.setattr_argument("Zeeman_Frequency", NumberValue(default = 70.0))
-        self.setattr_argument("Zeeman_Amplitude", NumberValue(default = 0.35)) 
+        self.setattr_argument("Zeeman_Amplitude", NumberValue(default = 0.09)) 
         # self.setattr_argument("Zeeman_Attenuation", NumberValue(default = 0.0))
 
         self.setattr_argument("RMOT_Frequency", NumberValue(default = 80.0))
-        self.setattr_argument("RMOT_Amplitude", NumberValue(default = 0.08)) 
+        self.setattr_argument("RMOT_Amplitude", NumberValue(default = 0.05)) 
         # self.setattr_argument("RMOT_Attenuation", NumberValue(default = 0.0))
 
-        self.setattr_argument("Probe_Frequency", NumberValue(default = 200))
-        self.setattr_argument("Probe_Amplitude", NumberValue(default = 0.17)) 
+        self.setattr_argument("Probe_Frequency", NumberValue(default = 200.0))
+        self.setattr_argument("Probe_Amplitude", NumberValue(default = 0.3)) 
         # self.setattr_argument("Probe_Attenuation", NumberValue(default = 0.0))
 
-        self.setattr_argument("Clock_Frequency", NumberValue(default = 85.0))
-        self.setattr_argument("Clock_Attenuation", NumberValue(default = 0.0))
+        self.settr_argument("lattice_frequency", NumberValue(default = 100.0))
+        self.settr_argument("lattice_attenuation_dB", NumberValue(default = 10))
+
+        self.setattr_argument("clock_frequency", NumberValue(default = 85.0))
+        self.setattr_argument("clock_attenuation_dB", NumberValue(default = 16)
         self.setattr_argument("Switch_off",BooleanValue(default=False))
 
     @kernel
@@ -64,6 +67,8 @@ class Everything_ON(EnvExperiment):
 
         # self.mot_coil_1.init()
         # self.mot_coil_2.init()
+
+        # initialise AD9910
         self.blue_mot_aom.cpld.init()
         self.blue_mot_aom.init()
         self.zeeman_slower_aom.cpld.init()
@@ -72,22 +77,32 @@ class Everything_ON(EnvExperiment):
         self.red_mot_aom.init()
         self.probe_aom.cpld.init()
         self.probe_aom.init()
+
+        # initialise AD9912
+        self.lattice_aom.cpld.init()
+        self.lattice_aom.init()
         self.atom_lock_aom.cpld.init()
+        self.atom_lock_aom.init()
+        self.stepping_aom.cpld.init()
         self.stepping_aom.init()
 
+
+        # Switch on all DDS channels
         self.blue_mot_aom.sw.on()
         self.zeeman_slower_aom.sw.on()
         self.red_mot_aom.sw.on()
         self.probe_aom.sw.on()
+        self.lattice_aom.sw,on()
         self.atom_lock_aom.sw.on()
         self.stepping_aom.sw.on()
 
+
+        # Setting attenuation for AD9910
         self.blue_mot_aom.set_att(0.0)
         self.zeeman_slower_aom.set_att(0.0)
         self.red_mot_aom.set_att(0.0)
         self.probe_aom.set_att(0.0)
-        self.stepping_aom.set_att(0.0)
-        self.atom_lock_aom.set_att(0.0)
+       
 
 
         # self.mot_coil_1.write_dac(0, 0.976)    
@@ -109,7 +124,11 @@ class Everything_ON(EnvExperiment):
 
         self.probe_aom.set(frequency=self.Probe_Frequency * MHz, amplitude=1.0)
 
-        self.stepping_aom.set(frequency=self.Clock_Frequency * MHz)
+        self.lattice_aom.set(frequency=self.lattice_frequency * MHz)
+        self.lattice_aom.set_att(self.lattice_attenuation * dB)
+
+        self.stepping_aom.set(frequency=self.clock_frequency * MHz)
+        self.stepping_aom.set_att(self.clock_attenuation_dB * dB)
 
         delay(1000*ms)
 
