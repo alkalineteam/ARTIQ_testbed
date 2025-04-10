ba34c10cdad7185d8b36248bcc4942975c24efd8
alkalineteam <alkalineteam88@gmail.com>
2025-04-10 12:01:27 +0100

    no coils

--- repository/Experimental Sequences/blue_mot_loading_time.py
+++ repository/Experimental Sequences/blue_mot_loading_time.py
@@ -1,10 +1,10 @@
 from artiq.experiment import *
 from artiq.coredevice.ttl import TTLOut
-from artiq.coredevice.sampler import Sampler 
+#from artiq.coredevice.sampler import Sampler 
 from numpy import int64
 from artiq.coredevice import ad9910
 
-import numpy as np 
+#import numpy as np 
 
 class bluemot_loading_time(EnvExperiment):
     def build(self):
@@ -18,15 +18,15 @@ class bluemot_loading_time(EnvExperiment):
         self.zeeman_slower_shutter:TTLOut=self.get_device("ttl6")
         self.probe_shutter:TTLOut=self.get_device("ttl7")
         self.camera_trigger:TTLOut=self.get_device("ttl8")
-        self.repump_shutter_679:TTLOut=self.get_device("ttl10")
+       # self.repump_shutter_679:TTLOut=self.get_device("ttl10")
         self.camera_shutter:TTLOut=self.get_device("ttl11")  
         #AD9910
         self.blue_mot_aom = self.get_device("urukul0_ch1")
         self.zeeman_slower_aom = self.get_device("urukul0_ch2")
         self.probe_aom = self.get_device("urukul0_ch3")          
         #Zotino
-        self.mot_coil_1=self.get_device("zotino0")
-        self.mot_coil_2=self.get_device("zotino0")
+        #self.mot_coil_1=self.get_device("zotino0")
+        #self.mot_coil_2=self.get_device("zotino0")
         
              
         self.setattr_argument("Cycles", NumberValue(default=10))
@@ -40,10 +40,10 @@ class bluemot_loading_time(EnvExperiment):
         self.blue_mot_shutter.output()
         self.zeeman_slower_shutter.output()
         self.repump_shutter_707.output()
-        self.repump_shutter_679.output()
+        #self.repump_shutter_679.output()
         self.probe_shutter.output()
-        self.mot_coil_1.init()
-        self.mot_coil_2.init()
+        #self.mot_coil_1.init()
+        #self.mot_coil_2.init()
         self.blue_mot_aom.cpld.init()
         self.blue_mot_aom.init()
         self.zeeman_slower_aom.cpld.init()
@@ -86,14 +86,14 @@ class bluemot_loading_time(EnvExperiment):
                 self.zeeman_slower_aom.sw.on()
                 
                 
-                voltage_1 = 7.95
-                voltage_2 = 8.0
-                self.mot_coil_1.write_dac(0, voltage_1)
-                self.mot_coil_2.write_dac(1, voltage_2)
+                #voltage_1 = 7.95
+                #voltage_2 = 8.0
+                #self.mot_coil_1.write_dac(0, voltage_1)
+                #self.mot_coil_2.write_dac(1, voltage_2)
 
-                with parallel:
-                    self.mot_coil_1.load()
-                    self.mot_coil_2.load()
+                #with parallel:
+                 #   self.mot_coil_1.load()
+                  #  self.mot_coil_2.load()
 
 
                 with parallel: 
@@ -101,7 +101,7 @@ class bluemot_loading_time(EnvExperiment):
                     self.probe_shutter.off()
                     self.zeeman_slower_shutter.on()
                     self.repump_shutter_707.on()
-                    self.repump_shutter_679.on()
+                    #self.repump_shutter_679.on()
 
 
                 delay(loading_time*ms)
